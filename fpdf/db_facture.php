a2e65d979afde948f9bf1e79ef63f4e0d1347ede
MedHediMaaloul <92804083+MedHediMaaloul@users.noreply.github.com>
2022-12-29 11:48:05 +0100

    update db facture

--- fpdf/db_facture.php
+++ fpdf/db_facture.php
@@ -1,16 +1,15 @@
 <?php
+$servername = 'Localhost';
+$username = 'root';
+$password = 'K2Location';
+$dbname = 'db_k2loc';
 
-session_start();
-include("../Gestion_location/inc/connect_db.php");
-
-global $conn;
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 $sqlQuery = "INSERT INTO facture_client (id_client,id_contrat,id_agence) 
             SELECT id_client, id_contrat, id_agence 
             FROM contrat_client AS C 
             WHERE ( C.date_fin > Now() AND C.date_debut < Now() AND DAY(C.date_debut) = DAY(NOW()) )
             OR ( C.date_fin = date(NOW()) )";
-
 $result = mysqli_query($conn, $sqlQuery);
-
 ?>
